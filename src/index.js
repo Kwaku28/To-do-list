47d99dfb4f3010d9604d8886656fba935b529b33
Lawrence Amoafo <rekenburg777@gmail.com>
2023-03-30 09:05:44 +0000

    Configure add items to list

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import './style.css';
+import TodoList from './modules/addtask.js';
+
+const taskList = document.querySelector('.task-container');
+const addTodo = document.querySelector('form');
+const description = document.querySelector('#new-task');
+const clearAll = document.querySelector('.completed');
+
+const todoList = new TodoList();
+clearAll.addEventListener('click', () => {
+  todoList.cleanCompleted();
+  todoList.setStorage();
+  todoList.displayToDo(taskList);
+});
+
+addTodo.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (description.value.trim()) {
+    todoList.addTask(description.value);
+    todoList.setStorage();
+    todoList.resetIndex();
+    todoList.displayToDo(taskList);
+    addTodo.reset();
+  }
+});
+
+document.addEventListener('click', (e) => {
+  if (e.target && e.target.classList.contains('delete')) {
+    const id = parseInt(e.target.parentElement.id, 5);
+    todoList.removeList(id);
+    todoList.resetIndex();
+    todoList.setStorage();
+    todoList.displayToDo(taskList);
+  }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  todoList.getStorage();
+  todoList.displayToDo(taskList);
+});
